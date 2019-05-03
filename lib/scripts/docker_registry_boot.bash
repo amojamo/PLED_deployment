680955efe43f363ab55e63190c8c822f88c6d79a
askilao <askilao@stud.ntnu.no>
2019-05-03 10:57:34 +0200

    docker registry boot

--- lib/scripts/docker_registry_boot.bash
+++ lib/scripts/docker_registry_boot.bash
@@ -8,7 +8,16 @@ sh get-docker.sh
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-#Clone PLED repository
-git clone https://github.com/amojamo/PLED /home/ubuntu/pled
+#clone git repo with config files
+git clone https://github.com/amojamo/PLED.git /home/ubuntu/
 
+#set docker-registry as working directory
+cd /home/ubuntu/pled/docker/docker-registry
+
+#create basic credentials
+mkdir -p /home/ubuntu/htpasswd_backup
+docker run --rm --entrypoint htpasswd registry:2 -Bbn root "poot" >~/htpasswd_backup/htpasswd
+
+#run compose and start registry stack 
+docker-compose -f docker-compose.yml -f docker-compose.auth.yml up -d --build
 
